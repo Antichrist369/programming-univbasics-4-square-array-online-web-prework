cba21bb68cb2b0a89779d698ad1e0cdf97461da5
Anthony Molina <antonio-molina@att.net>
2019-10-20 06:01:56 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  result = Array.new(array.size)
+  count = 0
+  while count < array.size  
+    result[count] = array[count] **2 
+    count += 1 
+  end 
+   return result 
 end
